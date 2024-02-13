c83102433848d9c8e6753a9954d48619ef18b50e
satyajit nayak <satyajitn15@gmail.com>
2024-02-13 08:18:43 +0530

    add interactivity

--- js/trigonometry.js
+++ js/trigonometry.js
@@ -5,28 +5,63 @@ const offset = {
   y: myCanvas.height / 2,
 };
 
-// move 0,0 to middle of canvas
-ctx.translate(offset.x, offset.y);
-
 const A = { x: 0, y: 0 };
 const B = { x: 90, y: 120 };
 const C = { x: B.x, y: 0 };
 
-drawCoordinateSystem(ctx, offset);
+// move 0,0 to middle of canvas
+ctx.translate(offset.x, offset.y);
 
-drawPoint(A);
-drawText('A', A);
-drawPoint(B);
-drawText('B', B);
-drawPoint(C);
-drawText('C', C);
+function update() {
+  // naming convetion AB opposite is C so name distance b/w AB as c
+  const c = distance(A, B);
+  const a = distance(B, C);
+  const b = distance(A, C);
 
-function drawText(text, loc, color = 'white') {
+  ctx.clearRect(-offset.x, -offset.y, myCanvas.width, myCanvas.height);
+
+  drawCoordinateSystem(ctx, offset);
+
+  drawLine(A, B);
+  drawText('c', average(A, B));
+  drawLine(A, C);
+  drawText('b', average(A, C));
+  drawLine(B, C);
+  drawText('a', average(B, C));
+
+  drawText('𝜃', A);
+}
+
+update();
+document.onmousemove = (event) => {
+  B.x = event.x - offset.x;
+  B.y = event.y - offset.y;
+
+  C.x = B.x;
+
+  update();
+};
+
+function average(p1, p2) {
+  return {
+    x: (p1.x + p2.x) / 2,
+    y: (p1.y + p2.y) / 2,
+  };
+}
+
+function distance(p1, p2) {
+  return Math.hypot(p1.x - p2.x, p1.y - p2.y);
+}
+
+function drawText(text, loc, color = 'black') {
   ctx.beginPath();
   ctx.fillStyle = color;
   ctx.textAlign = 'center';
-  ctx.textBaseLine = 'middle';
-  ctx.font = 'bold 13px Courier';
+  ctx.textBaseline = 'middle';
+  ctx.font = 'bold 18px Courier';
+  ctx.strokeStyle = 'white';
+  ctx.lineWidth = 7;
+  ctx.strokeText(text, loc.x, loc.y);
   ctx.fillText(text, loc.x, loc.y);
 }
 
@@ -35,6 +70,15 @@ function drawPoint(loc, size = 20, color = 'black') {
   ctx.fillStyle = color;
   ctx.arc(loc.x, loc.y, size / 2, 0, Math.PI * 2);
   ctx.fill();
+}
+
+function drawLine(p1, p2, color = 'black') {
+  ctx.beginPath();
+  ctx.strokeStyle = color;
+  ctx.lineWidth = 2;
+  ctx.moveTo(p1.x, p1.y);
+  ctx.lineTo(p2.x, p2.y);
+  ctx.stroke();
 }
 
 function drawCoordinateSystem(ctx, offset) {
