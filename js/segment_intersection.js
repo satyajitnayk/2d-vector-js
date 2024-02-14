8a5f83683dc4d0391e494f8282e4f25a4ec285b0
satyajit nayak <satyajitn15@gmail.com>
2024-02-14 22:55:01 +0530

    fix intersection issue

--- js/segment_intersection.js
+++ js/segment_intersection.js
@@ -7,10 +7,25 @@ const C = { x: 50, y: 100 };
 const D = { x: 250, y: 200 };
 
 const ctx = interCanvas.getContext('2d');
+let angle = 0;
+
+const mouse = { x: 0, y: 0 };
+
+document.onmousemove = (event) => {
+  mouse.x = event.x;
+  mouse.y = event.y;
+};
 
 animate();
 
 function animate() {
+  const radius = 50;
+  A.x = mouse.x + Math.cos(angle) * radius;
+  A.y = mouse.y - Math.sin(angle) * radius;
+  B.x = mouse.x - Math.cos(angle) * radius;
+  B.y = mouse.y + Math.sin(angle) * radius;
+  angle += 0.02;
+
   ctx.clearRect(0, 0, interCanvas.width, interCanvas.height);
 
   ctx.beginPath();
@@ -26,9 +41,9 @@ function animate() {
   drawDot(D, 'D');
 
   const I = getIntersection(A, B, C, D);
-  drawDot(I, 'I');
-
-  t += 0.005;
+  if (I) {
+    drawDot(I, 'I');
+  }
 
   requestAnimationFrame(animate);
 }
@@ -54,22 +69,32 @@ function getIntersection(A, B, C, D) {
 
    (Dx-Cx)*(Ay-Cy) - (Ax-Cx)(Dy-Cy) = (Bx-Ax)(Dy-Cy)t -(Dx-Cx)(By-Ay)t
 
-   => t = ((Dx-Cx)*(Ay-Cy) - (Ax-Cx)(Dy-Cy)) / ((Bx-Ax)(Dy-Cy) -(Dx-Cx)(By-Ay))
-   
+   => t = ((Dx-Cx)*(Ay-Cy) - (Ax-Cx)*(Dy-Cy)) / ((Bx-Ax)(Dy-Cy) -(Dx-Cx)(By-Ay))
+   => u = (Bx-Ax)*(Dy-Cy) - (Dx-Cx)* By - Ay) / ((Bx-Ax)(Dy-Cy) -(Dx-Cx)(By-Ay))
    FINAL value, using liner interpolation
-   Ix = A.x + (B.x - A.x) * t
-   Iy = A.y + (B.y - A.y) * t
+   Ix = Ax + (Bx - Ax) * t
+   Iy = Ay + (By - Ay) * t
    */
 
-  const numerator = (D.x - C.x) * (A.y - C.y) - (A.x - C.x) * (D.y - C.y);
-  const denominator = (B.x - A.x) * (D.y - C.y) - (D.x - C.x) * (B.y - A.y);
-  const t = numerator / denominator;
+  const tNumerator = (D.x - C.x) * (A.y - C.y) - (A.x - C.x) * (D.y - C.y);
+  const uNumerator = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
 
-  // do linear interpolation on either A-B or C-D to get intersection point
-  return {
-    x: A.x + (B.x - A.x) * t,
-    y: A.y + (B.y - A.y) * t,
-  };
+  const denominator = (B.x - A.x) * (D.y - C.y) - (D.x - C.x) * (B.y - A.y);
+  if (denominator != 0) {
+    const t = tNumerator / denominator;
+    const u = uNumerator / denominator;
+
+    // return only when lines intersect
+    if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+      // do linear interpolation on either A-B or C-D to get intersection point
+      return {
+        x: A.x + (B.x - A.x) * t,
+        y: A.y + (B.y - A.y) * t,
+        offset: t,
+      };
+    }
+  }
+  return null;
 }
 
 function lerp(a, b, t) {
