797da668060884c6d84f2234bc92919338174c81
satyajit nayak <satyajitn15@gmail.com>
2024-02-15 21:49:15 +0530

    add more methods to math.js

--- js/math.js
+++ js/math.js
@@ -59,3 +59,43 @@ function getIntersection(A, B, C, D) {
   }
   return null;
 }
+
+function distance(p1, p2) {
+  return Math.hypot(p1.x - p2.x, p1.y - p2.y);
+}
+
+function scale(p, scalar) {
+  return { x: p.x * scalar, y: p.y * scalar };
+}
+
+function add(p1, p2) {
+  return { x: p1.x + p2.x, y: p1.y + p2.y };
+}
+
+function subtract(p1, p2) {
+  return { x: p1.x - p2.x, y: p1.y - p2.y };
+}
+
+function dot(p1, p2) {
+  return p1.x * p2.x + p1.y * p2.y;
+}
+
+function normalize(p) {
+  return scale(p, 1 / magnitude(p));
+}
+
+function toPolar({ x, y }) {
+  return { dir: direction({ x, y }), mag: magnitude({ x, y }) };
+}
+
+function toXY({ mag, dir }) {
+  return { x: Math.cos(dir) * mag, y: Math.sin(dir) * mag };
+}
+
+function direction({ x, y }) {
+  return Math.atan2(y, x);
+}
+
+function magnitude({ x, y }) {
+  return Math.hypot(x, y);
+}
